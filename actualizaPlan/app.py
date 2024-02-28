6d376e7703b075b402abc0f0a7130e658aefc367
FunesRG <138267062+FunesRG@users.noreply.github.com>
2024-02-28 18:07:32 -0300

    Add files via upload
    Cargo documentos del servidor en flask con la página web y su estructura.
    También en gestor.py con datos y métodos necesarios.
    
    En estos momentos resuelvo inconvenientes con el DOM viertual de jscript al momento de recibir las materias del plan 2023.

---
+++ actualizaPlan/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from gestor import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/consultaMaterias', methods=['POST'])
+def consultaMaterias():
+    estados_plan2018 = request.json['estados']
+    cargarBD()
+    cargarEstados2018(estados_plan2018) 
+    estados_plan2023 = actualizarEstados2023()
+    
+    return estados_plan2023
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
